807f411fb35c6ee452842bc28df89361b409b543
Szymon Kownacki <szymon.kownacki@gmail.com>
2015-08-31 12:42:12 +0200

    Test custom deserializers

--- src/test/java/com/github/simonthecat/hom/HBaseObjectMapperTest.java
+++ src/test/java/com/github/simonthecat/hom/HBaseObjectMapperTest.java
@@ -6,7 +6,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.Test;
+
+import java.util.Date;
 
 import static org.apache.hadoop.hbase.util.Bytes.toBytes;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -40,6 +43,15 @@ public class HBaseObjectMapperTest {
 
     }
 
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PojoWithDate {
+
+        @CellDef(family = "f1", qualifier = "date")
+        private Date date;
+
+    }
 
     @Test
     public void mapperShouldMapAllFieldsUsingDefaultDeserializers() throws Exception {
@@ -63,5 +75,50 @@ public class HBaseObjectMapperTest {
         SimplePojo expected = new SimplePojo("rowkey01", "a string", 32L, 3.16f, 1.0, 99);
         assertThat(mapped).isEqualTo(expected);
 
+    }
+
+    @Test(expected = DeserializationException.class)
+    public void mapperShouldThrowIfDeserializerNotFound() throws Exception {
+        // given
+        HBaseObjectMapper mapper = HBaseObjectMapperBuilder.builder()
+                .withDefaultDeserializers()
+                .build();
+
+        // when
+        Date date = new Date(1451530800000L);
+
+        Result mockResult = mock(Result.class);
+        when(mockResult.getRow()).thenReturn(toBytes("rowkey01"));
+        when(mockResult.getValue(toBytes("f1"), toBytes("date"))).thenReturn(toBytes(date.getTime()));
+
+        mapper.map(mockResult, PojoWithDate.class);
+
+        // then expect DeserializationException
+    }
+
+    @Test
+    public void mapperShouldMakeUseOfCustomDeserializers() throws Exception {
+        // given
+        HBaseObjectMapper mapper = HBaseObjectMapperBuilder.builder()
+                .withDefaultDeserializers()
+                .registerTypeDeserializer(Date.class, new TypeDeserializer<Date>() {
+                    @Override
+                    public Date deserialize(byte[] bytes) throws DeserializationException {
+                        return new Date(Bytes.toLong(bytes));
+                    }
+                })
+                .build();
+
+        // when
+        Date date = new Date(1451530800000L);
+
+        Result mockResult = mock(Result.class);
+        when(mockResult.getRow()).thenReturn(toBytes("rowkey01"));
+        when(mockResult.getValue(toBytes("f1"), toBytes("date"))).thenReturn(toBytes(date.getTime()));
+
+        PojoWithDate mapped = mapper.map(mockResult, PojoWithDate.class);
+
+        // then
+        assertThat(mapped.getDate()).isEqualTo(date);
     }
 }
